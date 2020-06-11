e70e6be464d9f2f0900707eb663d70cf45c822f9
mbabali iryn <mbabaliiryn@gmail.com>
2020-06-11 12:15:17 +0300

    add mutual friends helper method

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -19,4 +19,21 @@ module FriendshipsHelper
     link = link_to 'Reject', user_friendship_path(current_user, user), method: :delete, class: 'add-friend'
     return link if current_user.friend_requests.map(&:id).include?(user.id)
   end
+
+  def mutual_friends(user)
+    return nil if user == current_user
+
+    m = current_user.friends.select { |u| user.friends.include?(u) }
+    m = m.delete(current_user) if m.include?(current_user)
+    if m.empty?
+      '0 mutual friends'
+    elsif m.size == 1
+      "#{m[0].name} is a mutual friend"
+    elsif m.size == 2
+      "#{m[0].name} and #{m[1].name} are mutual friends"
+    else
+      "#{m[0].name} and #{m.size - 1} others are mutual friends"
+
+    end
+  end
 end
